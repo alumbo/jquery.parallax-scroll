37223c685d7bd35dcd238d75be330e272058f5c8
Matthieu Chavigny <matttieu@gmail.com>
2015-03-05 11:36:07 +0100

    Keep original style attribute

--- js/jquery.parallax-scroll.js
+++ js/jquery.parallax-scroll.js
@@ -45,6 +45,26 @@ var ParallaxScroll = {
             var $el = $(el);
             var properties = [];
             var data = $el.data("parallax");
+            var style = $el.data("style");
+            if (style == undefined) {
+                style = $el.attr("style")||"";
+                $el.data("style", style);
+            }
+            var scrollFrom = data["from-scroll"];
+            if (scrollFrom == undefined) scrollFrom = Math.max(0, $(el).offset().top - windowHeight);
+            scrollFrom = scrollFrom | 0;
+            var scrollDistance = data["distance"];
+            var scrollTo = data["to-scroll"];
+            if (scrollDistance == undefined && scrollTo == undefined) scrollDistance = windowHeight;
+            if (scrollTo == undefined) scrollTo = scrollFrom + scrollDistance;
+            scrollTo = scrollTo | 0;
+            var smoothness = data["smoothness"];
+            if (smoothness == undefined) smoothness = 30;
+            smoothness = smoothness | 0;
+            if (noSmooth || smoothness == 0) smoothness = 1;
+            var scrollCurrent = scroll;
+            scrollCurrent = Math.max(scrollCurrent, scrollFrom);
+            scrollCurrent = Math.min(scrollCurrent, scrollTo);
             this._properties.map($.proxy(function(prop) {
                 var to = data[prop];
                 if (to == undefined) return;
@@ -52,21 +72,6 @@ var ParallaxScroll = {
                 var prev = $el.data("_" + prop);
                 if (prev == undefined) prev = 0;
                 prev = prev | 0;
-                var scrollFrom = data["from-scroll"];
-                if (scrollFrom == undefined) scrollFrom = Math.max(0, $(el).offset().top - windowHeight);
-                scrollFrom = scrollFrom | 0;
-                var scrollDistance = data["distance"];
-                var scrollTo = data["to-scroll"];
-                if (scrollDistance == undefined && scrollTo == undefined) scrollDistance = windowHeight;
-                if (scrollTo == undefined) scrollTo = scrollFrom + scrollDistance;
-                scrollTo = scrollTo | 0;
-                var smoothness = data["smoothness"];
-                if (smoothness == undefined) smoothness = 30;
-                smoothness = smoothness | 0;
-                if (noSmooth || smoothness == 0) smoothness = 1;
-                var scrollCurrent = scroll;
-                scrollCurrent = Math.max(scrollCurrent, scrollFrom);
-                scrollCurrent = Math.min(scrollCurrent, scrollTo);
                 var next = to * ((scrollCurrent - scrollFrom) / (scrollTo - scrollFrom));
                 next = Math.floor(next * this.round) / this.round;
                 var val = prev + (next - prev) / smoothness;
@@ -80,10 +85,12 @@ var ParallaxScroll = {
                 if (properties["z"] != undefined) {
 	                var perspective = data["perspective"];
 	                if (perspective == undefined) perspective = 800;
-                    $el.parent().attr("style", "perspective:" + perspective + "px; -webkit-perspective:" + perspective + "px");
+                    var $parent = $el.parent();
+                    if(!$parent.data("style")) $parent.data("style", $parent.attr("style")||"");
+                    $parent.attr("style", "perspective:" + perspective + "px; -webkit-perspective:" + perspective + "px; "+$parent.data("style"));
                 }
                 var translate3d = "translate3d(" + (properties["x"] ? properties["x"] : 0) + "px, " + (properties["y"] ? properties["y"] : 0) + "px, " + (properties["z"] ? properties["z"] : 0) + "px);";
-                $el.attr("style", "transform:" + translate3d + "; -webkit-transform:" + translate3d);
+                $el.attr("style", "transform:" + translate3d + "; -webkit-transform:" + translate3d+"; "+style);
             }
         }, this));
         if(window.requestAnimationFrame) {
